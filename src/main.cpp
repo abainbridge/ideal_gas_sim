1582b4eceb52da6277806b0e279ff82c23ddf2be
Andrew Bainbridge <a@b>
2023-01-13 20:43:01 +0000

    Improve benchmark accuracy by only including the advance time.

--- src/main.cpp
+++ src/main.cpp
@@ -20,23 +20,25 @@ int main(int argc, char *argv[]) {
     g_world.m_viewScale = (float)g_window->bmp->width / WORLD_SIZE_X;
     DfFont *font = LoadFontFromMemory(df_mono_8x15, sizeof(df_mono_8x15));
 
-    double startTime = GetRealTime();
-    double duration = 0.0;
-    unsigned frameNum = 0;
+    int frameNum = 0;
+    double totalAdvanceTime = 0.0;
     while (!g_window->windowClosed && !g_window->input.keys[KEY_ESC]) {
         BitmapClear(g_window->bmp, g_colourBlack);
         InputPoll(g_window);
 
+        double startTime = GetRealTime();
         g_world.Advance();
+        double duration = GetRealTime() - startTime;
+        if (frameNum < 500)
+            totalAdvanceTime += duration;
+
         g_world.Render(g_window->bmp);
 
         RectFill(g_window->bmp, g_window->bmp->width - 54, 0, 54, 13, g_colourBlack);
         DrawTextRight(font, g_colourWhite, g_window->bmp, g_window->bmp->width - 2, 0, "FPS:%i", g_window->fps);
 
-        if (frameNum < 500)
-            duration = GetRealTime() - startTime;
         RectFill(g_window->bmp, 0, 0, 130, 13, g_colourBlack);
-        DrawTextLeft(font, g_colourWhite, g_window->bmp, 2, 0, "Bench Time: %.2f", duration);
+        DrawTextLeft(font, g_colourWhite, g_window->bmp, 2, 0, "Bench Time: %.2f", totalAdvanceTime);
 
         UpdateWin(g_window);
 
